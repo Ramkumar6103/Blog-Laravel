3c14afbc444d5385ce33ba597b8cf61e0af9f56a
Ramkumar6103 <ramkumarb6103@gmail.com>
2025-07-03 00:05:53 +0530

    Updating view for index page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        return view('index');
+    }
+    public function post($id){
+        return "<h1>Post Page with parameter :".$id."</h1>";
+    }
+    // public function oldUrl(){
+    //     return redirect('/new-url');
+    // }
+        public function oldUrl(){
+        return redirect()->route('new_url');
+    }
+        public function newUrl(){
+        return "<h1>New url</h1>";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,21 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    // return view('welcome');
-    return "Hello World";
-});
+// Route::get('/', function () {
+//     // return view('welcome');
+//     //return "Hello World";
+// });
 
-Route::get('/index',function(){
-    return "<h1>This is Index page";
-});
+// Route::get('/index',function(){
+//     return "<h1>This is Index page";
+// });
+
+Route::get('/',[PostController::class,'index']);
+// Route::get('/',function(){
+//     return view('index');
+// });
+Route::get('/post/{id}',[PostController::class,'post'])->where('id','[0-9]+');
+Route::get('/old-url',[PostController::class,'oldUrl']);
+Route::get('/newest-url',[PostController::class,'newUrl'])->name('new_url');
